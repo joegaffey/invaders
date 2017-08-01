f5bf0cf28650cb4a0e4668dc780749a00a982f2b
System <none>
2017-08-01 14:03:10 +0000

    🚞💏 Refactored enemy class

--- public/enemy.js
+++ public/enemy.js
@@ -1,52 +1,55 @@
-function getEnemy(x, y) {
-  var enemy = new PIXI.Sprite(GameGraphics.getEnemyGraphics());
-  enemy.x = x;
-  enemy.y = y;
-  enemy.anchor.x = 0.5;
-  enemy.anchor.y = 0.5;
-  enemy.hits = 0;
-  enemy.ticker = new PIXI.ticker.Ticker();
-  enemy.ticker.add(function() {
-    enemy.rotation += enemy.hits * 0.1;
-    enemy.scale.x = 1 - (enemy.hits * 0.1);  
-    enemy.scale.y = 1 - (enemy.hits * 0.1);  
-  });
-  enemy.hit = function() {
+function Enemy(x, y) {
+  PIXI.Sprite.call(this, GameGraphics.getEnemyGraphics());
+  this.x = x;
+  this.y = y;
+  this.anchor.x = 0.5;
+  this.anchor.y = 0.5;
+  this.hits = 0;
+  
+  this.hit = function() {
     GameAudio.alienHitSound();
-    enemy.hits++;
-    if(enemy.hits == 5) {
-      enemy.ticker.stop();
-      enemy.destroy(); 
+    this.hits++;
+    if(this.hits == 5) {
+      this.ticker.stop();
+      this.destroy(); 
       return true;
     }
     return false;
   }
-  enemy.shoot = function() {
-    addEnemyBullet(enemy.x, enemy.y - enemy.height / 2);
+  
+  this.shoot = function() {
+    this.addEnemyBullet(this.x, this.y - this.height / 2);
   }
-  enemy.ticker.start();
-  return enemy;
+  
+  this.addEnemyBullet = function(x, y) {    
+    var bullet = new PIXI.Sprite(GameGraphics.getBulletGraphics());
+    bullet.x = x;
+    bullet.y = y;
+    bullet.anchor.x = 0.5;
+    bullet.anchor.y = 0.5;
+    bullet.speed = 6;
+    bullet.ticker = new PIXI.ticker.Ticker();
+    bullet.ticker.add(function() {
+      bullet.y += bullet.speed;
+      if(bullet.y > app.renderer.height) {
+        bullet.ticker.stop();
+        bullet.destroy();
+      }
+      else {
+        ship.checkHit(bullet);
+        grid.checkCellHit(bullet);
+      }
+    });
+    bullet.ticker.start();
+    app.stage.addChild(bullet);
+  }
+  
+  this.ticker = new PIXI.ticker.Ticker();
+  this.ticker.add(function() {
+    this.rotation += this.hits * 0.1;
+    this.scale.x = 1 - (this.hits * 0.1);  
+    this.scale.y = 1 - (this.hits * 0.1);  
+  }.bind(this));
+  this.ticker.start();
 }
-
-function addEnemyBullet(x, y) {    
-  var bullet = new PIXI.Sprite(GameGraphics.getBulletGraphics());
-  bullet.x = x;
-  bullet.y = y;
-  bullet.anchor.x = 0.5;
-  bullet.anchor.y = 0.5;
-  bullet.speed = 6;
-  bullet.ticker = new PIXI.ticker.Ticker();
-  bullet.ticker.add(function() {
-    bullet.y += bullet.speed;
-    if(bullet.y > app.renderer.height) {
-      bullet.ticker.stop();
-      bullet.destroy();
-    }
-    else {
-      ship.checkHit(bullet);
-      grid.checkCellHit(bullet);
-    }
-  });
-  bullet.ticker.start();
-  app.stage.addChild(bullet);
-}
+Enemy.prototype = Object.create(PIXI.Sprite.prototype);
