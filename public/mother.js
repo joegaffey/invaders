b8cef9af72cc490e758d1f0f1d464da6af9dcf7e
System <none>
2017-08-08 19:49:48 +0000

    🍧🌑 Added mothership animations

--- public/mother.js
+++ public/mother.js
@@ -6,13 +6,25 @@ class Mother extends PIXI.Sprite {
     this.anchor.x = 0.5;
     this.anchor.y = 0.5;
     this.hits = 0;
+    this.direction = 1;
+    this.ticker = new PIXI.ticker.Ticker();
+    this.ticker.add(function() {
+      if(Math.random() * 1000 < Props.MOTHER_FIRE_RATE)
+        this.shoot(this.x, this.y + this.height / 2);
+      this.x += this.direction * Props.MOTHER_SPEED;
+      if(this.x > Props.STAGE_HRES - this.width /2)
+        this.direction = -1;
+      if(this.x < this.width /2)
+        this.direction = 1;
+    }.bind(this));
+    this.ticker.start();
   }
   
   hit() {
     GameAudio.motherHitSound();
     this.hits++;
     if(this.hits === Props.MOTHER_MAX_HITS) {
-      //this.ticker.stop();
+      this.ticker.stop();
       Effects.explode(this.x, this.y, Props.EXPLOSION_HUGE);
       this.destroy(); 
       GameAudio.explosionSound();
@@ -35,26 +47,26 @@ class Mother extends PIXI.Sprite {
     }
   }
   
-  // addEnemyBullet(x, y) {    
-  //   var bullet = new PIXI.Sprite(GameGraphics.getBulletGraphics());
-  //   bullet.x = x;
-  //   bullet.y = y;
-  //   bullet.anchor.x = 0.5;
-  //   bullet.anchor.y = 0.5;
-  //   bullet.speed = Props.ENEMY_BULLET_SPEED;
-  //   bullet.ticker = new PIXI.ticker.Ticker();
-  //   bullet.ticker.add(function() {
-  //     bullet.y += bullet.speed;
-  //     if(bullet.y > app.renderer.height) {
-  //       bullet.ticker.stop();
-  //       bullet.destroy();
-  //     }
-  //     else {
-  //       ship.checkHit(bullet);
-  //       grid.checkCellHit(bullet);
-  //     }
-  //   });
-  //   bullet.ticker.start();
-  //   app.stage.addChild(bullet);
-  // }
+  shoot(x, y) {    
+    var bullet = new PIXI.Sprite(GameGraphics.getBulletGraphics());
+    bullet.x = x;
+    bullet.y = y;
+    bullet.anchor.x = 0.5;
+    bullet.anchor.y = 0.5;
+    bullet.speed = Props.ENEMY_BULLET_SPEED;
+    bullet.ticker = new PIXI.ticker.Ticker();
+    bullet.ticker.add(function() {
+      bullet.y += bullet.speed;
+      if(bullet.y > app.renderer.height) {
+        bullet.ticker.stop();
+        bullet.destroy();
+      }
+      else {
+        ship.checkHit(bullet);
+        grid.checkCellHit(bullet);
+      }
+    });
+    bullet.ticker.start();
+    app.stage.addChild(bullet);
+  }
 }
